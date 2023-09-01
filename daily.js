d48b45aabe842c4f05fa9682e2f9a8d8dc4ba2a5
Jorjay001 <143765296+Jorjay001@users.noreply.github.com>
2023-09-01 17:01:57 +0300

    Add files via upload

---
+++ daily.js
@@ -0,0 +1,111 @@
+function change() {
+     
+     var box= document.getElementById('box1');
+     
+     if(box.style.background==="red") {
+         
+         box.style.background = "green";
+     }else {
+         box.style.background = " red";
+     }
+     }
+     
+     function red() {
+         
+     
+     var box= document.getElementById('box2');
+     
+     if(box.style.background==="red") {
+         
+         box.style.background = "green";
+     }else {
+         box.style.background = " red";
+     }
+     }
+     
+     
+       function orange() {
+      var box=document.getElementById('box3');
+     
+     if(box.style.background==="red") {
+         
+         box.style.background = "green";
+     }else {
+         box.style.background = " red";
+     }
+     }
+     
+       function yellow() {
+      var box=document.getElementById('box4');
+     
+     if(box.style.background==="red") {
+         
+         box.style.background = "green";
+     }else {
+         box.style.background = " red";
+     }
+     }
+     
+       function green() {
+  var box=document.getElementById('box5');
+     
+     if(box.style.background==="red") {
+         
+         box.style.background = "green";
+     }else {
+         box.style.background = " red";
+     }
+     }
+     
+       function blue() {
+      var box=document.getElementById('box6');
+     
+     if(box.style.background==="red") {
+         
+         box.style.background = "green";
+     }else {
+         box.style.background = " red";
+     }
+     }
+     
+     
+       function indigo() {
+      var box=document.getElementById('box7');
+     
+     if(box.style.background==="red") {
+         
+         box.style.background = "green";
+     }else {
+         box.style.background = " red";
+     }
+     }
+     
+     
+       function violet() {
+      var box=document.getElementById('box8');
+     
+     if(box.style.background==="red") {
+         
+         box.style.background = "green";
+     }else {
+         box.style.background = " red";
+     }
+     }
+     
+       function black() {
+      var box=document.getElementById('box9');
+     
+     if(box.style.background==="red") {
+         
+         box.style.background = "green";
+     }else {
+         box.style.background = " red";
+     }
+}
+function message() {
+    alert('! CONGRATS YOU HAVE COMPLETED YOUR TASKS FOR THE DAY.')
+}
+function now() {
+    alert('!GOOD !MORNING TASKS COMPLETED..'.src="song.mp3"   );
+    document.getElementById('song').src="song.mp3";
+}
